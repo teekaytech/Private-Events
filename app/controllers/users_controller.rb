7170410e62628dd61c47013a2967f299c7902aad
teekaytech <taofeek.olalere@feghas.com>
2020-07-02 14:56:49 +0100

    Add index to show all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,28 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
-    @user = 
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
+    # @user = User.find(user_params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit([:username, :email])
   end
 end
