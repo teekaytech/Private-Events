87a02261344c8fd0f31b1ce17c170a4989c59b64
teekaytech <taofeek.olalere@feghas.com>
2020-07-07 09:29:29 +0100

    Show previous and upcoming events in event index page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,8 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
+    @past_events = Event.previous_events
+    @upcoming_events = Event.upcoming_events
   end
 
   def new
