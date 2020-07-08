50fe0cb547eb8ee2d5cb8de334ade79a27564a8b
teekaytech <taofeek.olalere@feghas.com>
2020-07-08 09:17:29 +0100

    Add test cases for user model attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true
   has_many :users_events
   has_many :attended_events, through: :users_events
   has_many :created_events, foreign_key: "user_id", class_name: "Event"
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(username: 'superuser', email: 'super@user.com') }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is valid when username is present' do
+    subject.username = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is valid with or without email' do
+    subject.email = nil
+    expect(subject).to be_valid
+  end
 end
